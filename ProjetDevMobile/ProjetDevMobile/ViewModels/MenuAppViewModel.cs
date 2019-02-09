3e13964e5f5742f47cab21c4588fb655e7df4c5f
loicmolina <loicmolina@outlook.com>
2019-02-09 09:29:30 +0100

    first commit

---
+++ ProjetDevMobile/ProjetDevMobile/ViewModels/MenuAppViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetDevMobile.ViewModels
+{
+	public class MenuAppViewModel : BindableBase
+	{
+        private INavigationService _navigationService;
+
+        public DelegateCommand NavigateAccueilCommand { get; private set; }
+
+        public MenuAppViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NavigateAccueilCommand = new DelegateCommand(ClickedAccueil);
+
+        }
+
+        private void ClickedAccueil()
+        {
+            _navigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+    }
+}
